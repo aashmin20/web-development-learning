731f429ea9ab6b40ff3f0ebfd20561e5c3efb370
Aashmin Rawal <aashminhere@gmail.com>
2026-06-04 15:56:10 +0530

    Added Day 2 HTML project and JavaScript practice

---
+++ javascript/js_day1_p1.js
@@ -0,0 +1,26 @@
+let myName = "Aashmin";
+console.log(myName);
+
+const myCountry = "India";
+console.log(myCountry);
+
+let currentAge = 19;
+console.log(currentAge);
+
+currentAge = 20;
+console.log(currentAge);
+
+const myCity = "Jaipur";
+// myCity = "Delhi";  // Gives an error because const values cannot be changed
+
+let city = "Jaipur";
+let course = "B.Tech CSE";
+let mentor = "Akhilesh";
+let college = "UPES";
+
+console.log("Name:", myName);
+console.log("Age:", currentAge);
+console.log("City:", city);
+console.log("Course:", course);
+console.log("Mentor:", mentor);
+console.log("College:", college);
