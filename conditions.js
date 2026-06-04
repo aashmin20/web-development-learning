c9ab4528d30dd3b6bb0e7954139f7e6fc37d6e16
Aashmin Rawal <aashminhere@gmail.com>
2026-06-04 16:21:51 +0530

    Added JavaScript conditions practice

---
+++ conditions.js
@@ -0,0 +1,44 @@
+let number = 10;
+
+if (number > 0) {
+    console.log("The number is positive");
+} else if (number < 0) {
+    console.log("The number is negative");
+} else {
+    console.log("The number is zero");
+}
+
+let age = 19;
+
+if (age >= 18) {
+    console.log("Eligible for voting");
+} else {
+    console.log("Not eligible for voting");
+}
+
+let marks = 75;
+
+if (marks > 40) {
+    console.log("Student passed");
+} else {
+    console.log("Student failed");
+}
+
+let value = 120;
+
+if (value > 100) {
+    console.log("Number is greater than 100");
+} else {
+    console.log("Number is not greater than 100");
+}
+
+let firstNumber = 25;
+let secondNumber = 18;
+
+if (firstNumber > secondNumber) {
+    console.log(firstNumber + " is larger");
+} else if (secondNumber > firstNumber) {
+    console.log(secondNumber + " is larger");
+} else {
+    console.log("Both numbers are equal");
+}
