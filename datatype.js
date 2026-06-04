604771d93d06a62154a6d0420e5bb333ed0f3152
Aashmin Rawal <aashminhere@gmail.com>
2026-06-04 16:19:15 +0530

    Added JavaScript data types practice

---
+++ datatype.js
@@ -0,0 +1,25 @@
+let studentName = "Aashmin";
+console.log(studentName);
+
+let studentAge = 19;
+console.log(studentAge);
+
+let isStudent = true;
+console.log(isStudent);
+
+let technologies = ["HTML", "CSS", "JavaScript", "Python"];
+console.log(technologies);
+
+let myDetails = {
+    name: "Aashmin",
+    age: 19,
+    city: "Jaipur"
+};
+
+console.log(myDetails);
+
+console.log(typeof studentName);
+console.log(typeof studentAge);
+console.log(typeof isStudent);
+console.log(typeof technologies);
+console.log(typeof myDetails);
