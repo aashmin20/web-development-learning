59a12c176a227bb8eddd1a03feae8fa56c3ba6d5
Aashmin Rawal <aashminhere@gmail.com>
2026-06-04 16:24:37 +0530

    Added JavaScript operators practice

---
+++ operators.js
@@ -0,0 +1,48 @@
+let num1 = 20;
+let num2 = 10;
+
+// Sum of two numbers
+let sum = num1 + num2;
+console.log("Sum:", sum);
+
+// Product of two numbers
+let product = num1 * num2;
+console.log("Product:", product);
+
+// Check if a number is even or odd
+let number = 15;
+
+if (number % 2 === 0) {
+    console.log(number + " is even");
+} else {
+    console.log(number + " is odd");
+}
+
+// Compare two numbers
+let firstNumber = 25;
+let secondNumber = 25;
+
+if (firstNumber === secondNumber) {
+    console.log("Both numbers are equal");
+} else {
+    console.log("Numbers are not equal");
+}
+
+// Check which number is greater
+let a = 50;
+let b = 30;
+
+if (a > b) {
+    console.log(a + " is greater");
+} else if (b > a) {
+    console.log(b + " is greater");
+} else {
+    console.log("Both numbers are equal");
+}
+
+// Find remainder
+let dividend = 17;
+let divisor = 5;
+
+let remainder = dividend % divisor;
+console.log("Remainder:", remainder);
